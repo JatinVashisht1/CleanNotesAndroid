5d71d70b9a63c99edebeaf74950cdedcdbd6d240
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-04-16 11:40:48 +0530

    Chore(documentation): Documented the code

--- app/src/main/java/com/jatinvashisht/cleannotes/di/AppModule.kt
+++ app/src/main/java/com/jatinvashisht/cleannotes/di/AppModule.kt
@@ -9,9 +9,22 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+/**
+ * Singleton Object containing methods to resolve dependencies.
+ *
+ * Installed in *SingletonComponent*, therefore available in whole application.
+ * */
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    /**
+     * Method to provide dependency of **NoteDatabase**.
+     *
+     * @param [app] Application object, provided by hilt.
+     *
+     * @return [NoteDatabase]
+     * */
     @Provides
     @Singleton
     fun provideNoteDatabase(app: Application): NoteDatabase =
